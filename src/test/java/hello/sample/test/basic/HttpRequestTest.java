52bea19ee79a5439e781ef450d12f11fca318e37
brandon low <brandonlow@ymail.com>
2021-09-19 20:12:32 +1000

    stuff to ignore error

--- src/test/java/hello/sample/test/basic/HttpRequestTest.java
+++ src/test/java/hello/sample/test/basic/HttpRequestTest.java
@@ -22,6 +22,7 @@ public class HttpRequestTest {
 
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
+		//restTemplate.get
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
 				String.class)).contains("Hello, World");
 	}
